6c13771cb9cf9e7d7e372d37d3439c9099791e5f
Hareendranath <hareendranath777@gmail.com>
2015-10-28 00:24:17 +0530

    Blog excerpt added

--- wp-content/themes/vilyoo/content.php
+++ wp-content/themes/vilyoo/content.php
@@ -30,7 +30,7 @@
 	</div><!-- .entry-summary -->
 	<?php else : ?>
 	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', '_tk' ) ); ?>
+		<?php the_excerpt(); ?>
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', '_tk' ),
