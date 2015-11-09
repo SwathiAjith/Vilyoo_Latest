7e1490cf32938dfadfe7e3b681f1d75b5b5cb718
Arjun S Kumar <arjun@brawnycode.com>
2015-11-10 02:44:16 +0530

    Home page mods.

--- wp-content/themes/vilyoo/templates/template-home-page-launch.php
+++ wp-content/themes/vilyoo/templates/template-home-page-launch.php
@@ -6,34 +6,61 @@
 get_header(); ?>
 
 <?php // substitute the class "container-fluid" below if you want a wider content area ?>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-md-12">
-				<div id="home-featured-seller">
-					<div class="col-md-9 white-bg pad-left pad-right shadow-it" id="home-featured-seller-left">
-						<div class="col-md-12 text-center">
-							<h2 class="hero-title">
-								<span>We're on a Mission,</span>
-								to popularize modern <strong><em>Crafting</em></strong> techniques.
-							</h2>
-							<a href="#" class="btn hero-btn">Learn More</a>
+	<div class="container-fluid pad-left pad-right homgBannerBg">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div id="home-featured-seller">
+						<div class="col-md-9 pad-left pad-right">
+<!--
+							<div class="col-md-12 text-center">
+								<h2 class="hero-title">
+									<span>We're on a Mission,</span>
+									to popularize modern <strong><em>Crafting</em></strong> techniques.
+								</h2>
+								<a href="#" class="btn hero-btn">Learn More</a>
+							</div>
+-->
 						</div>
-            		</div>
-                	<div class="col-md-3 pad-right" id="home-featured-seller-right">
-		            	<div class="col-xs-12 pad-left pad-right">
-		            		<div id="home-seller-info" class="text-center white-bg shadow-it col-md-12 pad-left pad-right">
-					           	<h3 class="sell-title">
-					           		Sell your crafts
-					           		<br>with<br>
-					           		<strong><em>Vilyoo!</em></strong>
-					           	</h3>
-					           	<a href="<?php echo get_site_url()?>/my-account/" class="btn btn-sellwithus">Start Selling!</a>
-					           	<p class="col-xs-12">
-					           		<small><em>*Sign up now to get early-stage seller benefits!</em></small>
-					           	</p>
-					        </div>
-				        </div>
-					</div><!-- col-md-4 -->
+						<div class="col-md-3 pad-right" id="home-featured-seller-right">
+							<div class="col-xs-12 pad-left pad-right">
+								<div id="home-seller-info" class="text-center white-bg col-md-12 pad-left pad-right">
+									<h3 class="sell-title">
+										Sell your crafts
+										<br>with<br>
+										<strong><em>Vilyoo!</em></strong>
+									</h3>
+									<a href="<?php echo get_site_url()?>/my-account/" class="btn btn-sellwithus">Start Selling!</a>
+									<p class="col-xs-12">
+										<small><em>*Sign up now to get early-stage seller benefits!</em></small>
+									</p>
+								</div>
+							</div>
+						</div><!-- col-md-4 -->
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="homeLinksToProduct">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 pad-left pad-right">
+						<div class="col-md-4 colProdLinks">
+							<p class="text-center homeBannerFooterLinks">
+								<a href="<?php echo get_site_url(); ?>/category/workshops/">Workshops</a>
+							</p>
+						</div>
+						<div class="col-md-4 colProdLinks">
+							<p class="text-center homeBannerFooterLinks">
+								<a href="<?php echo get_site_url(); ?>/category/diy-kits/">DIY Kits</a>
+							</p>
+						</div>
+						<div class="col-md-4 colProdLinks">
+							<p class="text-center homeBannerFooterLinks">
+								<a href="#">Supplies</a>
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -41,6 +68,7 @@ get_header(); ?>
 	<div class="container">
 		<div class="row">
 			<div id="content" class="main-content-inner col-sm-12">
+<!--
 				<div class="promoBanners">
 					<div class="col-md-8 pad-left pad-right">
 						<a href="#">
@@ -60,9 +88,10 @@ get_header(); ?>
 						</div>
 					</div>
 				</div>
+-->
 				<div id="home-most-popular">
 					<div class="vilyoo-section-header col-xs-12 mt-15">
-						<h4>Latest Items</h4>
+						<h4>Latest Products</h4>
 					</div>
 					<div class="white-bg col-md-12 shadow-it">
 						<?php // echo do_shortcode( '[best_selling_products per_page="5" columns="5"]' ); ?>
